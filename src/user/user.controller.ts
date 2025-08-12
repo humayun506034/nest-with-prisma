08ab100a5a336c13b97cb1c21f6b5eae9f9e0496
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2025-08-12 11:54:14 +0600

    modify

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
-import { UserService } from './user.service' 
+import { Controller, Get, Param, Patch, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
 @Controller('users')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Get()
-    async getAllUser(){
-        const result = await this.userService.getAllUsers();
-        return result;
-    }
+  @Get()
+  async getAllUser() {
+    const result = await this.userService.getAllUsers();
+    return result;
+  }
 
+  @Get(':id')
+  async getSingleUser(@Param('id') id: string) {
+    const result = await this.userService.getSingleUser(id);
+    return {
+      message: 'Single user fetched successfully..',
+      data: result,
+    };
+  }
+
+  @Patch(':id')
+  async updateUser(@Param('id') id: string, @Body() data: Partial<User>) {
+    // console.log({id, data});
+
+    const result = await this.userService.updateUser(id, data);
+    return {
+      message: 'User updated successfully...',
+      data:result
+    };
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import prisma from 'src/utils/prisma';
 
 @Injectable()
@@ -6,5 +7,27 @@ export class UserService {
   async getAllUsers() {
     const result = await prisma.user.findMany();
     return result;
+  }
+
+  async getSingleUser(id: string) {
+    const result = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return result;
+  }
+
+  async updateUser(id: string, data: Partial<User>) {
+    const findUser = await prisma.user.findFirst({ where: { id } });
+    if (!findUser) {
+      throw new Error('User not found!');
+    }
+
+    return await prisma.user.update({
+      where: { id },
+      data: data,
+    });
   }
 }
